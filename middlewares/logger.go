fc0bec1954c2331dfd3faf3ebd10027dac96eb80
Jagadeesh Nalla <siri.jagadeesh@gmail.com>
2023-11-25 20:24:39 +0530

    custom logger

--- middlewares/logger.go
+++ middlewares/logger.go
@@ -9,10 +9,43 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
+type logWritter struct {
+	http.ResponseWriter
+	wroteHeader bool
+	code        int
+	bytes       int
+}
+
+func (lwr *logWritter) WriteHeader(code int) {
+	if !lwr.wroteHeader {
+		lwr.code = code
+		lwr.wroteHeader = true
+		lwr.ResponseWriter.WriteHeader(code)
+	}
+}
+
+func (lwr *logWritter) Write(buf []byte) (int, error) {
+	lwr.maybeWriteHeader()
+	n, err := lwr.ResponseWriter.Write(buf)
+	lwr.bytes += n
+	return n, err
+}
+
+func (lwr *logWritter) maybeWriteHeader() {
+	if !lwr.wroteHeader {
+		lwr.WriteHeader(http.StatusOK)
+	}
+}
+
 // Logger middleware
 func Logger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
-		logRes := middleware.NewWrapResponseWriter(res, req.ProtoMajor)
+		logRes := logWritter{
+			ResponseWriter: res,
+			wroteHeader:    false,
+			code:           0,
+			bytes:          0,
+		}
 
 		start := time.Now()
 		defer func() {
@@ -23,11 +56,11 @@ func Logger(next http.Handler) http.Handler {
 				slog.String("proto", req.Proto),
 				slog.String("path", req.URL.Path),
 				slog.Duration("response_time", time.Since(start)),
-				slog.Int("status", logRes.Status()),
-				slog.Int("size", logRes.BytesWritten()),
+				slog.Int("status", logRes.code),
+				slog.Int("size", logRes.bytes),
 			)
 		}()
 
-		next.ServeHTTP(logRes, req)
+		next.ServeHTTP(&logRes, req)
 	})
 }
