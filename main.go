0fbbff58264d608bd2ea4dddc26569362cb21cd2
Jagadeesh Nalla <siri.jagadeesh@gmail.com>
2023-11-25 20:27:17 +0530

    change listen-addr to port

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	port := 0
-	flag.IntVar(&port, "listen-addr", 3008, "server listen port")
+	flag.IntVar(&port, "port", 3008, "server listen port")
 	flag.Parse()
 
 	rest.NewServer(port).Start()
